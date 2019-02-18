578a4036141e7b475a0fbc8e12133e54a4b637af
Panu Lahtinen <pnuu+git@iki.fi>
2019-02-18 10:29:48 +0200

    Move plugins to package init

--- trollflow2/__init__.py
+++ trollflow2/__init__.py
@@ -1,3 +1,62 @@
+from satpy import Scene
+from satpy.writers import compute_writer_results
+from logging import getLogger
+from posttroll.listener import ListenerContainer
+from six.moves.queue import Empty as queue_empty
+from six.moves.urllib.parse import urlparse
+#from multiprocessing import Process
+from posttroll.message import Message
+from collections import OrderedDict
+import dpath
+
+LOG = getLogger("trollflow2_plugins")
+
+
 class AbortProcessing(Exception):
-    def __init__(self, message, errors):
+    def __init__(self, message):
         super(AbortProcessing, self).__init__(message)
+
+
+def create_scene(job, reader=None):
+    LOG.info('Generating scene')
+    job['scene'] = Scene(filenames=job['input_filenames'], reader=reader)
+
+
+def load_composites(job):
+    composites = set(dpath.util.values(job['product_list'], '/product_list/*/products/*/productname'))
+    LOG.info('Loading %s', str(composites))
+    scn = job['scene']
+    scn.load(composites)
+    job['scene'] = scn
+
+
+def resample(job, radius_of_influence=None):
+    job['resampled_scenes'] = {}
+    scn = job['scene']
+    for area, config in job['product_list']['product_list'].items():
+        composites = dpath.util.values(config, '/products/*/productname')
+        LOG.info('Resampling %s to %s', str(composites), str(area))
+        job['resampled_scenes'][area] = scn.resample(area, composites, radius_of_influence=radius_of_influence)
+
+
+def save_datasets(job):
+    scns = job['resampled_scenes']
+    objs = []
+    for area, config in job['product_list']['product_list'].items():
+        for prod, pconfig in config['products'].items():
+            for fmat in pconfig['formats']:
+                cfmat = fmat.copy()
+                cfmat.pop('format', None)
+                objs.append(scns[area].save_dataset(pconfig['productname'], compute=False, **cfmat))
+    compute_writer_results(objs)
+
+
+class FilePublisher(object):
+    def __init__(self):
+        # initialize publisher
+        pass
+
+    def __call__(self, job):
+        # create message
+        # send message
+        pass
--- trollflow2/satpy_launcher.py
+++ trollflow2/satpy_launcher.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
-from satpy import Scene
-from satpy.utils import debug_on
-from satpy.writers import compute_writer_results
 from logging import getLogger
 from posttroll.listener import ListenerContainer
 from six.moves.queue import Empty as queue_empty
-from six.moves.urllib.parse import urlparse
 import sys
 #from multiprocessing import Process
 from threading import Thread as Process  # to get ipdb to work
 import yaml
 import time
 from trollmoves.utils import gen_dict_extract
-from posttroll.message import Message
 from collections import OrderedDict
-import dpath
 
 """The order of basic things is:
 - Create the scene
@@ -73,50 +67,6 @@ def message_to_job(msg, product_list):
     return job
 
 
-def create_scene(job, reader=None):
-    LOG.info('Generating scene')
-    job['scene'] = Scene(filenames=job['input_filenames'], reader=reader)
-
-
-def load_composites(job):
-    composites = set(dpath.util.values(job['product_list'], '/product_list/*/products/*/productname'))
-    LOG.info('Loading %s', str(composites))
-    scn = job['scene']
-    scn.load(composites)
-    job['scene'] = scn
-
-
-def resample(job, radius_of_influence=None):
-    job['resampled_scenes'] = {}
-    scn = job['scene']
-    for area, config in job['product_list']['product_list'].items():
-        composites = dpath.util.values(config, '/products/*/productname')
-        LOG.info('Resampling %s to %s', str(composites), str(area))
-        job['resampled_scenes'][area] = scn.resample(area, composites, radius_of_influence=radius_of_influence)
-
-
-def save_datasets(job):
-    scns = job['resampled_scenes']
-    objs = []
-    for area, config in job['product_list']['product_list'].items():
-        for prod, pconfig in config['products'].items():
-            for fmat in pconfig['formats']:
-                cfmat = fmat.copy()
-                cfmat.pop('format', None)
-                objs.append(scns[area].save_dataset(pconfig['productname'], compute=False, **cfmat))
-    compute_writer_results(objs)
-
-
-class FilePublisher(object):
-    def __init__(self):
-        # initialize publisher
-        pass
-
-    def __call__(self, job):
-        # create message
-        # send message
-        pass
-
 def process(msg, prod_list):
     with open(prod_list) as fd:
         config = yaml.load(fd.read())
@@ -126,8 +76,8 @@ def process(msg, prod_list):
         cwrk.pop('fun')(job, **cwrk)
 
 
-
 def main():
+    from satpy.utils import debug_on
     debug_on()
     topics = sys.argv[1].split(',')
     prod_list = sys.argv[2]
