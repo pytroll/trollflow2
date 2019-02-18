a171d5ef693df54e4bf3e70568b10a48001afc0f
Panu Lahtinen <pnuu+git@iki.fi>
2019-02-18 10:33:03 +0200

    Move coverage calculations to package init

--- trollflow2/__init__.py
+++ trollflow2/__init__.py
@@ -1,5 +1,10 @@
 from satpy import Scene
 from satpy.writers import compute_writer_results
+from satpy.resample import get_area_def
+try:
+    from trollsched.satpass import Pass
+except ImportError:
+    Pass = None
 from logging import getLogger
 from posttroll.listener import ListenerContainer
 from six.moves.queue import Empty as queue_empty
@@ -13,8 +18,7 @@ LOG = getLogger("trollflow2_plugins")
 
 
 class AbortProcessing(Exception):
-    def __init__(self, message):
-        super(AbortProcessing, self).__init__(message)
+    pass
 
 
 def create_scene(job, reader=None):
@@ -60,3 +64,32 @@ class FilePublisher(object):
         # create message
         # send message
         pass
+
+
+def coverage(job):
+    """Check area coverage"""
+    if Pass is None:
+        LOG.error("Trollsched import failed, coverage calculation not possible")
+        return
+    min_coverage = config.get('min_coverage')
+    if not min_coverage:
+        LOG.debug("Minimum area coverage not given or set to zero")
+        return
+    area = config['area']
+    platform_name = scn_mda['platform_name']
+    start_time = scn_mda['start_time']
+    end_time = scn_mda['end_time']
+    sensor = scn_mda['sensor']
+    cov = get_scene_coverage()
+    if cov < min_coverage:
+        raise AbortProcessing(
+            "Area coverage %.2f %% below threshold %.2f %" % (cov,
+                                                              min_coverage))
+
+
+def get_scene_coverage(platform_name, start_time, end_time, sensor, area_id):
+    """Get scene area coverage in percentages"""
+    overpass = Pass(platform_name, start_time, end_time, instrument=sensor)
+    area_def = get_area_def(area_id)
+
+    return 100 * overpass.area_coverage(area_def)
