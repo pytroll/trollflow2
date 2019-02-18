faccbff210a401596d080a06140bf602cc6dc177
Martin Raspaud <martin.raspaud@smhi.se>
2019-02-18 10:41:47 +0100

    Add licenses

--- trollflow2/__init__.py
+++ trollflow2/__init__.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019 Pytroll developers
+
+# Author(s):
+
+#   Martin Raspaud <martin.raspaud@smhi.se>
+#   Panu Lahtinen <pnuu+git@iki.fi>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
 from satpy import Scene
 from satpy.writers import compute_writer_results
 from satpy.resample import get_area_def
@@ -6,9 +30,6 @@ try:
 except ImportError:
     Pass = None
 from logging import getLogger
-from posttroll.listener import ListenerContainer
-from six.moves.queue import Empty as queue_empty
-from six.moves.urllib.parse import urlparse
 #from multiprocessing import Process
 from posttroll.message import Message
 from collections import OrderedDict
