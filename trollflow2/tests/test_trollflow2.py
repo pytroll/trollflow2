0b7363a174fd3ee7c01ef58398fd52c1f9640828
Panu Lahtinen <pnuu+git@iki.fi>
2019-02-20 12:19:25 +0200

    Add platform checking plugin

--- trollflow2/tests/test_trollflow2.py
+++ trollflow2/tests/test_trollflow2.py
@@ -307,6 +307,22 @@ class TestCovers(unittest.TestCase):
         area_coverage.assert_called_with(6)
 
 
+class TestCheckPlatform(unittest.TestCase):
+
+    @mock.patch('trollflow2.get_config_value')
+    def test_check_platform(self, get_config_value):
+        from trollflow2 import check_platform
+        from trollflow2 import AbortProcessing
+        get_config_value.return_value = None
+        job = {'product_list': None, 'input_mda': {'platform_name': 'foo'}}
+        self.assertIsNone(check_platform(job))
+        get_config_value.return_value = ['foo', 'bar']
+        self.assertIsNone(check_platform(job))
+        get_config_value.return_value = ['bar']
+        with self.assertRaises(AbortProcessing):
+            check_platform(job)
+
+
 def suite():
     """The test suite for test_writers."""
     loader = unittest.TestLoader()
@@ -318,6 +334,7 @@ def suite():
     my_suite.addTest(loader.loadTestsFromTestCase(TestLoadComposites))
     my_suite.addTest(loader.loadTestsFromTestCase(TestResample))
     my_suite.addTest(loader.loadTestsFromTestCase(TestCovers))
+    my_suite.addTest(loader.loadTestsFromTestCase(TestCheckPlatform))
 
     return my_suite
 
