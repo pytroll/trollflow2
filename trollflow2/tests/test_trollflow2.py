8ebb9d8281aa18528d1568f6a1eb6bf7c027a5e7
Martin Raspaud <martin.raspaud@smhi.se>
2019-11-11 14:30:43 +0100

    Add a test to ensure the `produced_files` queue is used

--- trollflow2/tests/test_trollflow2.py
+++ trollflow2/tests/test_trollflow2.py
@@ -496,6 +496,15 @@ class TestSaveDatasets(TestCase):
         }
         self.assertDictEqual(dexpected, job['product_list']['product_list'])
 
+        filenames = ['/tmp/satdmz/pps/www/latest_2018/NOAA-15_20190217_0600_euron1_in_fname_ctth_static.png',
+                     '/tmp/satdmz/pps/www/latest_2018/NOAA-15_20190217_0600_euron1_in_fname_ctth_static.jpg',
+                     '/tmp/satdmz/pps/www/latest_2018/NOAA-15_20190217_0600_euron1_in_fname_ct_and_ctth.nc',
+                     '/tmp/satdmz/pps/www/latest_2018/noaa15/20190217_0600_germ_in_fname_cloudtype_in_fname.png',
+                     '/tmp/NOAA-15_20190217_0600_omerc_bb_ct.nc',
+                     '/tmp/NOAA-15_20190217_0600_omerc_bb_cloud_top_height.tif']
+        for fname, efname in zip(the_queue.put.mock_calls, filenames):
+            self.assertEqual(fname, mock.call(efname))
+
 
 class TestCreateScene(TestCase):
     """Test case for creating a scene."""
