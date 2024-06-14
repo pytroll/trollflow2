f2701dbf0119780768b780fa721222b166f14d35
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-06-14 11:02:20 +0300

    Simplify cache storage option passing

--- trollflow2/plugins/__init__.py
+++ trollflow2/plugins/__init__.py
@@ -1125,18 +1125,10 @@ def use_fsspec_cache(job):
     cache_dir = job["product_list"].get("fsspec_cache_dir")
     filenames = job["input_filenames"]
     cached_filenames = [f"{cache}::{f}" for f in filenames]
+    kwargs = {}
     if cache_dir:
-        if cache == "blockcache":
-            open_files = fsspec.open_files(cached_filenames,
-                                           blockcache={"cache_storage": cache_dir})
-        elif cache == "filecache":
-            open_files = fsspec.open_files(cached_filenames,
-                                           filecache={"cache_storage": cache_dir})
-        elif cache == "simplecache":
-            open_files = fsspec.open_files(cached_filenames,
-                                           simplecache={"cache_storage": cache_dir})
-    else:
-        open_files = fsspec.open_files(cached_filenames)
+        kwargs[cache] = {"cache_storage": cache_dir}
+    open_files = fsspec.open_files(cached_filenames, **kwargs)
     fs_files = [FSFile(open_file) for open_file in open_files]
     job["input_filenames"] = fs_files
 
